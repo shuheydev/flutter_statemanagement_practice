9fcf96df200fa87beed88ccba1a305d0dd1660b7
shuheydev <shuhey.dev@gmail.com>
2020-07-29 17:32:41 +0900

    MySliderをstatelessに変更した.

--- lib/widgets/myslider.dart
+++ lib/widgets/myslider.dart
@@ -2,20 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_statemanagement_practice/schedule.dart';
 
-class MySlider extends StatefulWidget {
+class MySlider extends StatelessWidget {
   @override
-  _MySliderState createState() => _MySliderState();
-}
-
-class _MySliderState extends State<MySlider> {
-  var _value = 0.5;
-
-  void _onValueChanged(double value) {
-    setState(() {
-      _value = value;
-    });
-    print(value);
-  }
+  // _MySliderState createState() => _MySliderState();
 
   @override
   Widget build(BuildContext context) {
@@ -26,3 +15,23 @@ class _MySliderState extends State<MySlider> {
     );
   }
 }
+
+// class _MySliderState extends State<MySlider> {
+//   var _value = 0.5;
+
+//   void _onValueChanged(double value) {
+//     setState(() {
+//       _value = value;
+//     });
+//     print(value);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final schedule = Provider.of<MySchedule>(context);
+//     return Slider(
+//       onChanged: (value) => schedule.stateManagementTime = value,
+//       value: schedule.stateManagementTime,
+//     );
+//   }
+// }
